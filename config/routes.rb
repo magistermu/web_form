3b824f3848b8abc86de02826438129c9b52af472
magistermu <david.dccr@gmail.com>
2017-01-16 12:12:16 -0500

    Create basic form

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users_controller/new'
-
-  get 'users_controller/create'
-
   resources :users, only: [:new, :create]
 end
