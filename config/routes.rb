906db92c6a23a792ad3ac62b948ca017bf42e2b7
magistermu <david.dccr@gmail.com>
2017-01-15 22:00:37 -0500

    Generate Users Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
   get 'users_controller/new'
 
   get 'users_controller/create'
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
-class UsersControllerControllerTest < ActionDispatch::IntegrationTest
+class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_controller_new_url
+    get users_new_url
     assert_response :success
   end
 
   test "should get create" do
-    get users_controller_create_url
+    get users_create_url
     assert_response :success
   end
 
